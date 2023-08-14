1317cd94c59875432d9b95e220627058f6f1a12c
Bryan Olandres <ozoneblacklight@outlook.com>
2023-08-14 18:18:29 +0800

    added guard clause to stop iteration if the 'pages' does not exists anymore

--- the-exporter/utils/requestor.py
+++ the-exporter/utils/requestor.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import os
 from dotenv import load_dotenv
 
@@ -31,7 +32,7 @@ def generate_checkout():
     print(f'Response Data: {r.text}')
 
 
-# thingy
+# fetch a list of transaction from a starting page
 def fetch_transactions(page: int):
     load_dotenv()
     access_token = os.getenv('ACCESS_TOKEN')
@@ -45,13 +46,31 @@ def fetch_transactions(page: int):
         'entityId': '8a8294174b7ecb28014b9699220015ca',
         'date.from': '2023-08-14 00:00:00',
         'date.to': '2023-08-14 23:59:59',
-        'pageNo': str(page)
+        'pageNo': page
     }
 
     r = requests.get(url=url, params=params, headers=headers)
 
-    print(f'HTTP Status Code: {r.status_code}')
-    print(f'Response Data: {r.text}')
+    # response_data = r.text
+    parsed_data = json.loads(r.text)
+
+    if 'pages' in parsed_data:
+        page_count = parsed_data.get('pages')
+        print(f'Fetching page {page}... http code: {r.status_code}')
+
+        # todo: dump data to csv
+        # print(response_data)
+
+        print(f'Showing page {page} of {page_count} pages')
+
+        if page <= int(page_count):
+            next_page = page + 1
+            fetch_transactions(next_page)
+        else:
+            print('End of pages.')
+
+    else:
+        print(parsed_data)
 
 
 if __name__ == '__main__':
